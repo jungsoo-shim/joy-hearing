b0083b739becafe52eb64685273d21f6c68d3d6c
Jungsoo Shim <79438838+jungsoo-shim@users.noreply.github.com>
2022-11-10 11:19:45 -0500

    favicon

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Joy Hearing</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/public/favicon.ico" />
       </Head>
 
       <main>
