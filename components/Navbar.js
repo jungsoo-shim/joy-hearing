7124d4fd6ab7b14ca4200c69414cf7e743974afa
Jungsoo <79438838+jungsoo-shim@users.noreply.github.com>
2023-01-24 09:59:50 -0500

    mod: Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,19 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Navbar() {
  return (
   <>
-   <nav className='flex items-center flex-wrap bg-black text-white p-3'>
+   <nav className='flex items-center space-x-4 flex-wrap bg-black text-white p-3'>
+    <Image src="/joy-hearing-logo.svg" width={40} height={40}/>
     <Link href='/'>
      Home
+    </Link>
+    <Link href='/'>
+     About
+    </Link>
+    <Link href='/'>
+     Contact
     </Link>
    </nav>
   </>
