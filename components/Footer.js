46497e2275c6ec01d60f4d840fc05a70a6a54e94
Jungsoo <79438838+jungsoo-shim@users.noreply.github.com>
2023-01-31 13:30:38 -0500

    mod: Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <>
-      <footer className='h-10 flex items-center flex-wrap bg-black text-white p-3'>
+      <footer className='bg-white shadow px-2 py-3 flex justify-center items-center'>
         Copyright &copy; 2022 Joy Hearing
       </footer>
     </>
