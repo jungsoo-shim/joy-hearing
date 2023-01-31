321dbddfd0420f9a107eeb376367f434f5e1682b
Jungsoo <79438838+jungsoo-shim@users.noreply.github.com>
2023-01-31 15:03:21 -0500

    mod: layout

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import Footer from './Footer'
 export default function Layout({ children }) {
     return (
 	<>
-	    <div class="bg-sky-200 flex flex-col h-screen justify-between">
+	    <div class="bg-sky-400 flex flex-col h-screen justify-between">
 	    <Navbar />
 	    <main>{children}</main>
 	    <Footer />
