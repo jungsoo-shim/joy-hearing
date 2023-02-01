6c63c98b58822eb36093650362d6d894c33ff148
Jungsoo <79438838+jungsoo-shim@users.noreply.github.com>
2023-01-31 19:01:45 -0500

    mod: index, footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <>
       <footer className='bg-white shadow px-2 py-3 flex justify-center items-center'>
-        Copyright &copy; 2022 Joy Hearing Aid
+        Copyright &copy; 2022 Joy Hearing Aid Centre
       </footer>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,13 @@ export default function Home() {
   return (
     <div class="container">
       <Head>
-        <title>Joy Hearing</title>
+        <title>Joy Hearing Aid Centre</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="px-4">
         <div className="bg-white px-4">
-        <h1>Joy Hearing</h1>
+        <h1>Joy Hearing Aid Centre</h1>
         <p>Site is under construction</p>
         <p>Please call 416-512-9292 for services</p>
         </div>
