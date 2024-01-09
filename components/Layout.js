09ef0982def0467519d7c7b6bc602d97c512cd82
Jungsoo <79438838+jungsoo-shim@users.noreply.github.com>
2024-01-09 16:37:29 -0500

    changing website to SPA and updating

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import Footer from './Footer'
 export default function Layout({ children }) {
     return (
 	<>
-	    <div class="bg-slate-400 flex flex-col h-screen justify-between">
+	    <div className="bg-slate-400 flex flex-col h-screen justify-between">
 	    <Navbar />
 	    <main>{children}</main>
 	    <Footer />
