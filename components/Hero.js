528e080d25c38a79afcbb1596d11b08a28d0617b
jungsoo-shim <jungsoo.shim.dev@gmail.com>
2024-02-01 02:06:54 -0500

    update

--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,6 @@
 export default function Hero() {
     return (
-        <div className="flex items-center justify-center bg-gradient-to-r from-slate-500 to-slate-500 h-4/5 w-screen">
+        <div className="bg-contains bg-center bg-fixed bg-[url('../public/placeholder.png')] flex items-center justify-center h-4/5 w-screen">
             <div className="text-center">
                 <h1 className="text-5xl font-bold text-white">Experience the joy of hearing</h1>
             </div>
