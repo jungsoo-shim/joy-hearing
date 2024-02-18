7df9465d825ea01c840629886317be155f52cf31
Jungsoo <79438838+jungsoo-shim@users.noreply.github.com>
2024-02-17 21:06:33 -0500

    updated

--- components/BurgerMenuIcon.js
+++ components/BurgerMenuIcon.js
@@ -1,9 +1,9 @@
 export default function BurgerMenuIcon() {
     return (
-        <div className="p-2 mx-1">
-        <div className="w-8 h-1 my-1.5 mx-0 bg-slate-600"></div>
-        <div className="w-8 h-1 my-1.5 mx-0 bg-slate-600"></div>
-        <div className="w-8 h-1 my-1.5 mx-0 bg-slate-600"></div>
+        <div className="px-2 mx-1">
+        <div className="w-8 h-0.5 my-2 mx-0 bg-slate-600"></div>
+        <div className="w-8 h-0.5 my-2 mx-0 bg-slate-600"></div>
+        <div className="w-8 h-0.5 my-2 mx-0 bg-slate-600"></div>
         </div>
     )
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -3,6 +3,12 @@ import Image from "next/image";
 import BurgerMenuIcon from '@components/BurgerMenuIcon';
 
 export default function Navbar() {
+
+ function toggleMenu() {
+     var menu = document.getElementById("mobile-links");
+     (menu.style.display === "block") ? menu.style.display = "none" : menu.style.display = "block"; 
+ }
+
  return (
   <>
    <nav className="w-screen position: fixed bg-white shadow px-2 py-3 flex justify-between items-center">
@@ -13,8 +19,16 @@ export default function Navbar() {
      <h1 className="text-xl font-mono">Joy Hearing Aid Centre</h1>
     </div>
 
-    <div className="md:hidden hover:bg-slate-200">
-     <button>
+    <div id="mobile-links" className="flex flex-col">
+     <a href="#home"></a>
+     <a href="#about"></a>
+     <a href="#services"></a>
+     <a href="#pricing"></a>
+     <a href="#contact"></a>
+    </div>
+
+    <div className="flex items-center md:hidden hover:bg-slate-200">
+     <button onclick="toggleMenu()">
      <BurgerMenuIcon />
      </button>
     </div>
@@ -39,7 +53,6 @@ export default function Navbar() {
      <a className="px-3 py-2 hover:bg-slate-200" href="#contact">
       Contact
      </a>
-
     </div>
 
    </nav>
