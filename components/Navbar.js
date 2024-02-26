15516c8a936418892e7cc0e20142656050210d27
jungsoo-shim <jungsoo.shim.dev@gmail.com>
2024-02-26 15:51:42 -0500

    fix the navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,3 @@
-import a from "next/link";
-import Image from "next/image";
 import BurgerMenuIcon from '@components/BurgerMenuIcon';
 
 export default function Navbar() {
@@ -11,45 +9,45 @@ export default function Navbar() {
 
  return (
   <>
-   <nav className="w-screen position: fixed bg-white shadow px-2 py-3 flex flex-col">
+   <nav className="w-screen position: fixed bg-white shadow flex flex-col">
 
     <div className="flex justify-between items-center">
-        <div className="flex items-center">
+        <div className="flex py-3 items-center">
          <img className="h-10 px-3" src="/joy-hearing-logo.svg"/>
 
          <h1 className="text-xl font-mono">Joy Hearing Aid Centre</h1>
         </div>
 
-        <div className="flex items-center md:hidden hover:bg-slate-200">
+        <div className="py-3 flex items-center md:hidden hover:bg-slate-200">
          <button onClick={toggleMenu}>
          <BurgerMenuIcon />
          </button>
         </div>
         
-        <div className="max-md:hidden flex">
-         <a className="px-3 py-2 hover:bg-slate-200" href="#home">
+        <div className="mx-2 max-md:hidden flex">
+         <a className="px-3 py-5 hover:bg-slate-200" href="#home">
           Home
          </a>
 
-         <a className="px-3 py-2 hover:bg-slate-200" href="#about">
+         <a className="px-3 py-5 hover:bg-slate-200" href="#about">
           About
          </a>
 
-         <a className="px-3 py-2 hover:bg-slate-200" href="#services">
+         <a className="px-3 py-5 hover:bg-slate-200" href="#services">
           Services
          </a>
 
-         <a className="px-3 py-2 hover:bg-slate-200" href="#pricing">
+         <a className="px-3 py-5 hover:bg-slate-200" href="#pricing">
           Pricing
          </a>
 
-         <a className="px-3 py-2 hover:bg-slate-200" href="#contact">
+         <a className="px-3 py-5 hover:bg-slate-200" href="#contact">
           Contact
          </a>
         </div>
     </div>
 
-    <div id="mobile-links" className="hidden bg-slate-400 flex flex-col px-3 py-2">
+    <div id="mobile-links" className="hidden bg-slate-400 flex-col px-3 py-2">
      <div><a href="#home">Home</a></div>
      <div><a href="#about">About</a></div>
      <div><a href="#services">Services</a></div>
