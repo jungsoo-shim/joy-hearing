6dc1650e94cd61a7ab1378e729c6b9b0f4cf0326
jungsoo-shim <jungsoo.shim.dev@gmail.com>
2024-03-02 22:13:42 -0500

    update

--- components/About.js
+++ components/About.js
@@ -1,7 +1,14 @@
 export default function About() {
     return (
         <div className="container">
-            <p>This is About</p>
+            <h1>About</h1>
+            <p>Hello, I'm James Shim.</p>
+            <p>Seoul National University Institute of Technology, Department of Mechanical and Aerospace Engineering</p>
+            <p>KAIST Department of Mechanical Engineering (Ph.D)</p>
+            <p>Cornell University (Post Doctorate)</p>
+            <p>KIST Department of Mechanical Engineering (Senior Training Center)</p>
+            <p>George Brown College</p>
+            <p>17 years of hearing aid experience</p>
         </div>
     )
 }
