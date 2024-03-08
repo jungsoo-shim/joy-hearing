1cf3d99944bc6f2085e69e86c5334a0459fed8c8
Jungsoo <79438838+jungsoo-shim@users.noreply.github.com>
2024-03-08 09:09:22 -0500

    update

--- components/Services.js
+++ components/Services.js
@@ -1,17 +1,29 @@
 export default function Services() {
     return (
         <div id="services" className="bg-stone-300 h-screen flex flex-col items-center justify-center">
-            <h1>Service 1</h1>
+            <h1>Hearing Assessment</h1>
             <img></img>
             <p>Description 1</p>
 
-            <h1>Service 2</h1>
+            <h1>Hearing Aid Solutions</h1>
             <img></img>
             <p>Description 2</p>
 
-            <h1>Service 3</h1>
+            <h1>Ear Wax Removal</h1>
             <img></img>
             <p>Description 3</p>
+
+            <h1>Hearing Aid Adjustments</h1>
+            <img></img>
+            <p>Description 4</p>
+
+            <h1>Hearing Aid Repair</h1>
+            <img></img>
+            <p>Description 5</p>
+
+            <h1>Custom Ear Plugs</h1>
+            <img></img>
+            <p>Description 6</p>
         </div>
     )
 }
