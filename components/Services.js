6fcc27c25e99ae57ee05ba80865c82c795b091f5
jungsoo-shim <jungsoo.shim.dev@gmail.com>
2024-03-08 09:29:14 -0500

    message

--- components/Services.js
+++ components/Services.js
@@ -1,29 +1,46 @@
 export default function Services() {
     return (
-        <div id="services" className="bg-stone-300 h-screen flex flex-col items-center justify-center">
+        <div id="services" className="bg-stone-300 h-screen flex flex-col items-center justify-evenly">
+            
+            <div className="flex items-center justify-evenly w-screen">
+            <div className="flex flex-col items-center">
             <h1>Hearing Assessment</h1>
-            <img></img>
+            <img className="max-w-32" src="joy-hearing-logo.svg"></img>
             <p>Description 1</p>
+            </div>
 
+            <div className="flex flex-col items-center">
             <h1>Hearing Aid Solutions</h1>
-            <img></img>
+            <img className="max-w-32" src="joy-hearing-logo.svg"></img>
             <p>Description 2</p>
+            </div>
 
-            <h1>Ear Wax Removal</h1>
-            <img></img>
+            <div className="flex flex-col items-center">
+            <h1>Hearing Aid Maintenance</h1>
+            <img className="max-w-32" src="joy-hearing-logo.svg"></img>
             <p>Description 3</p>
+            </div>
+            </div>
 
-            <h1>Hearing Aid Adjustments</h1>
-            <img></img>
-            <p>Description 4</p>
-
+            <div className="flex items-center justify-evenly w-screen">
+            <div className="flex flex-col items-center">
             <h1>Hearing Aid Repair</h1>
-            <img></img>
-            <p>Description 5</p>
+            <img className="max-w-32" src="joy-hearing-logo.svg"></img>
+            <p>Description 4</p>
+            </div>
 
+            <div className="flex flex-col items-center">
             <h1>Custom Ear Plugs</h1>
-            <img></img>
+            <img className="max-w-32" src="joy-hearing-logo.svg"></img>
+            <p>Description 5</p>
+            </div>
+
+            <div className="flex flex-col items-center">
+            <h1>Ear Cleaning</h1>
+            <img className="max-w-32" src="joy-hearing-logo.svg"></img>
             <p>Description 6</p>
+            </div>
+            </div>
         </div>
     )
 }
