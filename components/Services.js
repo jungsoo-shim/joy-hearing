1fd862acbb8d2b17f12ca7ff957ded9bbbe73b22
jungsoo-shim <jungsoo.shim.dev@gmail.com>
2024-03-19 21:52:48 -0400

    update

--- components/Services.js
+++ components/Services.js
@@ -1,6 +1,7 @@
 export default function Services() {
     return (
-        <div id="services" className="p-10 text-slate-600 font-bold text-xl bg-stone-300 flex">
+        <div id="services" className="p-10 text-slate-600 font-bold text-xl bg-stone-300 flex justify-evenly item-center">
+            <div className="p-10">
             <ul>
             <li>- Hearing Assessment</li>
             <li>- Hearing Aid Solutions</li>
@@ -9,6 +10,9 @@ export default function Services() {
             <li>- Custom Ear Plugs</li>
             <li>- Ear Cleaning</li>
             </ul>
+            </div>
+            <img className="w-1/4 h-1/4 m-4" src="hearing-test.jpg"></img>
+            <img className="w-1/4 h-1/4 m-4" src="oticon.png"></img>
         </div>
     )
 }
