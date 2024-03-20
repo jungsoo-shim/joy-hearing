67d0721d083e6b02dcfa212d9aa2ae685f3cc945
jungsoo-shim <jungsoo.shim.dev@gmail.com>
2024-03-19 22:04:51 -0400

    update

--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,6 @@
 export default function Hero() {
     return (
-        <div id="home" className="grayscale flex w-screen h-dvh items-center justify-center bg-no-repeat bg-center bg-fixed bg-[url('../public/mall-floor-elevator.jpg')]">
+        <div id="home" className="grayscale flex w-screen h-dvh items-center justify-center bg-no-repeat bg-center bg-fixed bg-[url('../public/office-1.jpg')]">
             <div className="text-center">
                 <h1 className="text-5xl font-bold text-white">Experience the joy of hearing</h1>
             </div>
